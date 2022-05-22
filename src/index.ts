d742a1c76c9b82d2b13e1f90000664abc106df5a
Kuba Karpierz <mr.karpierz@gmail.com>
2022-05-21 21:16:11 -0400

    Moved canvas code to TS

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import { MountPlanner } from "./MountPlanner";
 import { Memory } from "./Memory";
 import { Update } from "./Update";
 import { UI } from "./UI";
+import { Canvas } from "./Canvas";
 
 const rng = new UniformRNG();
 
@@ -28,6 +29,7 @@ const mount = new Mount(perlin, tree, arch, polyTools);
 const mountPlanner = new MountPlanner(perlin, memory);
 const update = new Update(memory, mountPlanner, mount, perlin, arch);
 const ui = new UI(memory, update, seed);
+const canvas = new Canvas(perlin);
 
 // We add global variables at the end to ensure that we don't inadvertidly depend on them in our Typescript.
 InitializeGlobalVariables(rng, seed, perlin, polyTools, tree, mount, arch, man, mountPlanner, memory, update, ui);
